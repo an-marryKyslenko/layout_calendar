5bd9f5150244a0002662e5df1705e31fdf0023d3
an-marryKyslenko <olih.a@icloud.com>
2024-09-07 15:31:45 +0200

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,109 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Calendar_initial_state.png",
+        "test": "..\\bitmaps_test\\20240907-152956\\Calendar_initial_state.png",
+        "selector": "body",
+        "fileName": "Calendar_initial_state.png",
+        "label": "Calendar initial state",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.5,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/calendar/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.36989847819010413,
+          "misMatchPercentage": "0.37",
+          "analysisTime": 46
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Calendar_day_with_hover_effect.png",
+        "test": "..\\bitmaps_test\\20240907-152956\\Calendar_day_with_hover_effect.png",
+        "selector": "body",
+        "fileName": "Calendar_day_with_hover_effect.png",
+        "label": "Calendar day with hover effect",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.5,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/calendar/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.370025634765625,
+          "misMatchPercentage": "0.37",
+          "analysisTime": 40
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Calendar_starting_from_Wednesday.png",
+        "test": "..\\bitmaps_test\\20240907-152956\\Calendar_starting_from_Wednesday.png",
+        "selector": "body",
+        "fileName": "Calendar_starting_from_Wednesday.png",
+        "label": "Calendar starting from Wednesday",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.5,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/calendar/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.4355112711588542,
+          "misMatchPercentage": "0.44",
+          "analysisTime": 42
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Calendar_with_length_of_29_days.png",
+        "test": "..\\bitmaps_test\\20240907-152956\\Calendar_with_length_of_29_days.png",
+        "selector": "body",
+        "fileName": "Calendar_with_length_of_29_days.png",
+        "label": "Calendar with length of 29 days",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.5,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/calendar/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.447845458984375,
+          "misMatchPercentage": "0.45",
+          "analysisTime": 39
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
